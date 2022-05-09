5f815bba4dde35aadfa50856454c3510656d9e8c
Riobush32 <riobush32@gmail.com>
2022-05-09 09:57:20 +0700

    Functioanl Component

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, Image, TextInput } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <View style={{ width:80, height:80, backgroundColor:'salmon' }}/>
+      <Text>Hello World</Text>
+      <Photo/>
+      <Text>Hello World</Text>
+      <Home/>
+      <Text>Hello World</Text>
+      <TextInput style={{ borderWidth:1 }} />
+    </View>
+  );
+};
+
+const Home = () => {
+  return <Text>This Is Home Component!</Text>;
+};
+
+const Photo = () => {
+  return (
+    <Image
+      source={{ uri: 'https://placeimg.com/100/100/arch' }}
+      style={{ width: 100, height: 100, }} 
+    />
+  );
+};
+
+export default App;
